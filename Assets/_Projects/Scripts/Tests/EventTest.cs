e1ff2e2df554165f41a00de16fdd32efd55a9cdf
Eric Luo <yil295@UCSD.EDU>
2017-02-21 23:41:26 -0800

    -

---
+++ Assets/_Projects/Scripts/Tests/EventTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest : MonoBehaviour {
+
+
+
+    public string m1 = "Event 1 has been triggered";
+    public string m2 = "Event 2 has been triggered";
+
+
+    public void Event1()
+    {
+        Debug.Log(m1);
+    }
+
+
+    public void Event2()
+    {
+        Debug.Log(m2);
+    }
+}
