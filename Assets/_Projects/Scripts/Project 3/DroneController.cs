8526ea413313a7c369948d53a84861f0999b3af7
Eric Luo <yil295@UCSD.EDU>
2017-02-22 19:35:12 -0800

    drone controller- wip

---
+++ Assets/_Projects/Scripts/Project 3/DroneController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+using Leap;
+
+
+public class DroneController : MonoBehaviour {
+
+    public DroneState state = DroneState.Stop;
+    public float flySpeed = 4.0f;
+    [Header("Hands")]
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    private Vector3 direction = Vector3.zero;
+    private LeapProvider provider;
+    private CharacterController c_controller;
+
+	// Use this for initialization
+	void Start () {
+        c_controller = GetComponent<CharacterController>();
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        if (!provider)
+            Debug.LogError("Leap Provider not found");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // if (state == DroneState.Stop) return;
+
+        if(state == DroneState.Forward)
+        {
+            c_controller.Move(direction * flySpeed);
+        }
+	}
+
+    public void MoveWhereLeftHandisPointingAt()
+    {
+        Debug.Log("Left hand event called");
+        state = DroneState.Forward;
+        direction = leftHand.transform.forward;
+    }
+
+    public void MoveWhereRightHandisPointingAt()
+    {
+        Debug.Log("right hand event called");
+        state = DroneState.Forward;
+        direction = rightHand.transform.forward;
+
+        Frame frame = provider.CurrentFrame;
+        foreach (Hand hand in frame.Hands)
+        {
+            if (hand.IsLeft) continue;
+
+            List<Finger> fingers = hand.Fingers;
+            foreach (Finger finger in fingers)
+            {
+                Bone[] bones = finger.bones;
+                foreach (Bone bone in bones)
+                {
+
+                    Vector v = bone.Direction;
+
+                }
+            }
+        }
+
+
+
+    }
+
+
+    public void StopMoving()
+    {
+        state = DroneState.Stop;
+    }
+
+    public void FillerEvent1()
+    {
+        Debug.Log("Filler Event 1 Called");
+    }
+
+
+    public void FillerEvent2()
+    {
+        Debug.Log("Filler Event 2 Called");
+    }
+}
+
+
+public enum DroneState
+{
+    Forward, Stop
+}
---
+++ Assets/_Projects/Scripts/Tests/LeapGestureTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class LeapGestureTest : MonoBehaviour {
+    private LeapProvider provider;
+
+    // Use this for initialization
+    void Start () {
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        if (!provider)
+            Debug.LogError("Leap Provider not found");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Frame frame = provider.CurrentFrame;
+        int counter = 1;
+        foreach(Hand hand in frame.Hands)
+        {
+            
+            List<Finger> fingers =  hand.Fingers;
+            Debug.Log(" Hand #" + counter + " has " + fingers.Count + " fingers");
+            counter++;
+        }
+	}
+}
