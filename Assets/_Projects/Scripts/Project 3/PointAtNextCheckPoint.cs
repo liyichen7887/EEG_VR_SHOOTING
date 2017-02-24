200bcad83727dcfdd59006e5cec71d80aa366b03
Eric Luo <yil295@UCSD.EDU>
2017-02-24 00:54:09 -0800

    hooooooooooooooooooooo

--- Assets/_Projects/Scripts/Project 3/PointAtNextCheckPoint.cs
+++ Assets/_Projects/Scripts/Project 3/PointAtNextCheckPoint.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PointAtNextCheckPoint : MonoBehaviour {
 
     
-    public Transform Player;
+   // public Transform Player;
     public DroneController dc;
     public LoadCheckPoints lcp;
-    private Transform target;
+    public Transform target;
     // Use this for initialization
     void Start () {
 		
@@ -16,15 +16,13 @@ public class PointAtNextCheckPoint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        int n = dc.nextTargetCheckPoint;
+       int n = dc.nextTargetCheckPoint;
         if (n > lcp.checkPoints.Count) return;
 
-        target = lcp.checkPoints[n];
-      //  Debug.Log(" target checkPoint #:" + n);
-        Vector3 lookPos = target.position - Player.position;
-        lookPos.y = 0;
-        Quaternion rotation = Quaternion.LookRotation(lookPos);
-        this.transform.rotation = rotation;
-        transform.LookAt(target.position);
+        target = lcp.checkPoints[n-1];
+        transform.LookAt(target);
+
+
+
     }
 }
